5d6343dc4fdd99a72846e617900f89b9eba9d308
Lars Kellogg-Stedman <lars@oddbit.com>
2019-01-21 23:38:00 -0500

    doxygen comment updates

--- bool.h
+++ bool.h
@@ -1,3 +1,9 @@
+/**
+ * \file bool.h
+ *
+ * a simple replacement for stdbool.h. simavr and/or simulavr + gdb does not
+ * like the data types described in stdbool.h, so we use this instead.
+ */
 #ifndef _bool_h
 #define _bool_h
 
--- sim/simavr.c
+++ sim/simavr.c
@@ -1,8 +1,19 @@
+/**
+ * \file simavr.c
+ *
+ * This file describdes traces for simavr to collect. The metadata is stored in 
+ * the .mmcu section of the compiled ELF binary.
+ */
 #include <simavr/avr/avr_mcu_section.h>
 #include "pins.h"
 
 extern uint8_t state;
 
+/** Trace data to collect.
+ *
+ * We collect each bit of PORTB (inputs and outputs) separately. We also keep
+ * track of the global state variable.
+ */
 const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("PIN_POWER"),    .mask = (1<<PIN_POWER),    .what = (void*)&PINB,  },
     { AVR_MCU_VCD_SYMBOL("PIN_USB"),      .mask = (1<<PIN_USB),      .what = (void*)&PINB,  },
