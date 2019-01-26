a352fb04576031f18de18c96d796ad699071a738
Lars Kellogg-Stedman <lars@oddbit.com>
2019-01-25 23:59:10 -0500

    work on gpio chardev interface

--- pipowerd/pipowerd.c
+++ pipowerd/pipowerd.c
@@ -1,26 +1,49 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
+/**
+ * \file pipowerd.c
+ *
+ * Monitor GPIO for the SHUTDOWN signal.
+ */
+#define _POSIX_C_SOURCE 200809L
+
+#include <errno.h>
+#include <fcntl.h>
 #include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <wiringPi.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
-#define DEFAULT_PIN_SHUTDOWN 17
-#define DEFAULT_PIN_BOOT 4
+#include <linux/gpio.h>
+
+#ifndef DEFAULT_GPIO_DEV
+#define DEFAULT_GPIO_DEV "/dev/gpiochip0"
+#endif
+
+#ifndef DEFAULT_PIN
+#define DEFAULT_PIN 17
+#endif
+
+#ifndef DEFAULT_SHUTDOWN_COMMAND
 #define DEFAULT_SHUTDOWN_COMMAND "/bin/systemctl poweroff"
+#endif
 
-#define OPT_PIN_BOOT 'b'
-#define OPT_PIN_SHUTDOWN 's'
+#define OPT_GPIO_DEV 'd'
+#define OPT_PIN 'p'
 #define OPT_SHUTDOWN_COMMAND 'c'
 #define OPT_VERBOSE 'v'
 #define OPT_IGNORE_INITIAL_STATE 'i'
 #define OPT_HELP 'h'
 
-#define OPTSTRING "b:s:c:vih"
+#define OPTSTRING "d:p:c:vih"
 
 const struct option longopts[] = {
-    {"pin-shutdown", required_argument, 0, OPT_PIN_SHUTDOWN},
-    {"pin-boot", required_argument, 0, OPT_PIN_BOOT},
+    {"gpio-device", required_argument, 0, OPT_GPIO_DEV},
+    {"gpio-pin", required_argument, 0, OPT_PIN},
     {"shutdown-command", required_argument, 0, OPT_SHUTDOWN_COMMAND},
     {"ignore-initial-state", required_argument, 0, OPT_IGNORE_INITIAL_STATE},
     {"verbose", no_argument, 0, OPT_VERBOSE},
@@ -28,8 +51,9 @@ const struct option longopts[] = {
 };
 
 struct config {
-    int pin_shutdown,
-        pin_boot,
+    char *device;
+
+    int pin,
         verbose;
 
     bool ignore_initial_state;
@@ -40,8 +64,8 @@ struct config {
 bool flag_shutdown = false;
 
 void init_config() {
-    config.pin_shutdown = DEFAULT_PIN_SHUTDOWN;
-    config.pin_boot = DEFAULT_PIN_BOOT;
+    config.device = DEFAULT_GPIO_DEV;
+    config.pin = DEFAULT_PIN;
     config.verbose = 0;
     config.shutdown_command = DEFAULT_SHUTDOWN_COMMAND;
 }
@@ -50,37 +74,92 @@ void handle_shutdown() {
 	flag_shutdown = true;
 }
 
-void setup_pins() {
-    pinMode(config.pin_shutdown, INPUT);
-    pullUpDnControl(config.pin_shutdown, PUD_DOWN);
-    pinMode(config.pin_boot, OUTPUT);
-
-    wiringPiISR (config.pin_shutdown, INT_EDGE_RISING, handle_shutdown);
-}
-
-void set_boot() {
-    if (config.verbose > 0)
-	    printf("pipower: asserting BOOT signal\n");
-    digitalWrite(config.pin_boot, 0);
-}
-
-void reset_boot() {
-    if (config.verbose > 0)
-	    printf("pipower: de-asserting BOOT signal\n");
-    digitalWrite(config.pin_boot, 1);
-}
-
 bool check_shutdown() {
-        int val;
-        val = digitalRead(config.pin_shutdown);
-        if (config.verbose > 1)
-            printf("pipower: initial shutdown state = %d\n", val);
-        return val;
 }
 
 void usage(FILE *out) {
-    fprintf(out, "pipower: usage: pipower [-b <pin_boot>] [-s <pin_shutdown>] "
+    fprintf(out, "pipower: usage: pipower [-d <device>] [-p <pin>] "
                  "[-c <shutdown_command> ] [-vi]\n");
+}
+
+void delay(long ms) {
+    struct timespec ts;
+
+    ts.tv_sec = (ms/1000);
+    ts.tv_nsec = (ms-((ms/1000)*1000)) * 1000000;
+    nanosleep(&ts, NULL);
+}
+
+void monitor_shutdown_pin() {
+    struct gpioevent_request req;
+    struct gpiohandle_data data;
+    int fd;
+    int ret;
+
+    fd = open(config.device, 0);
+    if (fd == -1) {
+        ret = -errno;
+        fprintf(stderr, "pipower: failed to open %s: %s\n",
+                config.device, strerror(errno));
+        exit(ret);
+    }
+
+    req.lineoffset = config.pin;
+    req.handleflags = 0;
+    req.eventflags = GPIOEVENT_REQUEST_RISING_EDGE;
+    strcpy(req.consumer_label, "pipower-shutdown");
+
+    ret = ioctl(fd, GPIO_GET_LINEEVENT_IOCTL, &req);
+    if (ret == -1) {
+        ret = -errno;
+        fprintf(stderr, "pipower: failed to GET_LINEEVENT: %s\n",
+                strerror(errno));
+        exit(ret);
+    }
+
+    ret = ioctl(req.fd, GPIOHANDLE_GET_LINE_VALUES_IOCTL, &data);
+    if (ret == -1) {
+        ret = -errno;
+        fprintf(stderr, "pipower: failed to GET_LINE_VALUES: %s\n",
+                strerror(errno));
+        exit(ret);
+    }
+
+    if (data.values[0]) {
+        if (config.ignore_initial_state) {
+            if (config.verbose > 0)
+                printf("pipower: ignoring active shutdown request\n");
+
+        } else {
+            printf("pipower: shutdown request is already active\n");
+            return;
+        }
+    }
+
+    while (1) {
+        struct gpioevent_data event;
+
+        ret = read(req.fd, &event, sizeof(event));
+        if (ret == -1) {
+            if (errno == -EAGAIN) {
+                continue;
+            } else {
+                ret = -errno;
+                fprintf(stderr, "pipower: failed to read event: %s\n",
+                        strerror(errno));
+                exit(ret);
+            }
+        }
+
+        if (ret != sizeof(event)) {
+                ret = -EIO;
+                fprintf(stderr, "pipower: failed to read event: %s\n",
+                        strerror(errno));
+                exit(ret);
+        }
+
+        break;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -89,22 +168,17 @@ int main(int argc, char *argv[]) {
     int shutdown_state;
 
     init_config();
-    wiringPiSetupGpio();
 
     while (EOF != (ch = getopt_long(argc, argv, OPTSTRING, longopts, &option_index))) {
         switch (ch) {
-            case OPT_PIN_SHUTDOWN:
-                config.pin_shutdown = atoi(optarg);
-                if (config.pin_shutdown == 0) {
-                    printf("pipower: invalid shutdown pin specification: %s\n", optarg);
-                    exit(1);
-                }
+            case OPT_GPIO_DEV:
+                config.device = strdup(optarg);
                 break;
 
-            case OPT_PIN_BOOT:
-                config.pin_boot = atoi(optarg);
-                if (config.pin_boot == 0) {
-                    printf("pipower: invalid boot pin specification: %s\n", optarg);
+            case OPT_PIN:
+                config.pin = atoi(optarg);
+                if (config.pin == 0) {
+                    printf("pipower: invalid shutdown pin specification: %s\n", optarg);
                     exit(1);
                 }
                 break;
@@ -134,31 +208,16 @@ int main(int argc, char *argv[]) {
     }
 
     if (config.verbose > 0)
-	    printf("pipower: starting, shutdown=%d boot=%d\n", config.pin_shutdown, config.pin_boot);
+	    printf("pipower: starting, device=%s pin=%d\n",
+                config.device, config.pin);
 
-    setup_pins();
+    monitor_shutdown_pin();
 
-    if (check_shutdown()) {
-        printf("pipower: shutdown flag is set at boot%s\n",
-                config.ignore_initial_state?" (ignoring)":"");
-        if (!config.ignore_initial_state)
-            goto shutdown;
-    }
-
-    set_boot();
-
-    while(!flag_shutdown) {
-        delay(10);
-    }
-
-    reset_boot();
-
-shutdown:
     if (config.verbose > 0)
 	    printf("pipower: received shutdown signal\n");
-
-    if (config.verbose > 1)
-        printf("pipower: running shutdown command: %s\n", config.shutdown_command);
+        if (config.verbose > 1)
+            printf("pipower: running shutdown command: %s\n",
+                    config.shutdown_command);
 
     system(config.shutdown_command);
     return 0;
